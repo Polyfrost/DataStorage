cb27acee45075503938ada68f85cac8134a0f369
Jacob <kathund@proton.me>
2026-03-03 12:04:55 +0800

    fix: bundle generating being diff every time (#112)
    * refactor: exporting mrpack
    
    this uses the inbuilt export path function so it's "better"
    
    * fix: bundle generating being diff every time
    
    Co-authored-by: mat <git@matdoes.dev>
    
    * fix: workflows running on the wrong branch
    
    * chore(update-bundles)
    
    ---------
    
    Co-authored-by: mat <git@matdoes.dev>

--- oneclient/update-bundles.sh
+++ oneclient/update-bundles.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
-URL="https://nightly.link/Polyfrost/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
-ZIP="Linux-64-bit-x86.zip"
 
 echo "Downloading and setting up packwiz"
+URL="https://nightly.link/Polyfrost/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
+ZIP="Linux-64-bit-x86.zip"
 wget -O "$ZIP" "$URL" && unzip -o "$ZIP" && chmod +x packwiz
 
 for version in mrpacks/*; do
