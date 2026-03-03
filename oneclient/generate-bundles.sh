8e753c455d89ed871e28e057ff6cb2f69dbd23b5
Jacob <kathund@proton.me>
2026-03-03 08:08:17 +0800

    refactor: exporting mrpack
    this uses the inbuilt export path function so it's "better"

--- oneclient/generate-bundles.sh
+++ oneclient/generate-bundles.sh
@@ -15,17 +15,8 @@ for version in mrpacks/*; do
   for bundle in "$version"/*; do
     [ -d "$bundle" ] || continue
     echo "Bundling $bundle"
-    (cd "$bundle" && ../../../packwiz modrinth export)
-    export_files=("$bundle"/*.mrpack)
-    if [[ ${#export_files[@]} -ne 1 ]] || [[ ! -f "${export_files[0]}" ]]; then
-        echo "Error: Expected 1 .mrpack file in '$bundle', but found ${#export_files[@]}. Skipping." >&2
-        continue
-    fi
-    export_file="${export_files[0]}"
-
     name=$(basename "$bundle")
     name="${name,,}"
-    echo "Moving '$export_file' to 'generated/$name-$parsed.mrpack'"
-    mv "$export_file" "generated/$name-$parsed.mrpack"
+    (cd "$bundle" && ../../../packwiz modrinth export --output ../../../generated/$name-$parsed.mrpack)
   done
 done
