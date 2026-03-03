4fc2ce4efae0bdd3a022d3cdff6ae70e3de09b91
Jacob <kathund@proton.me>
2026-03-03 08:54:18 +0800

    fix: bundle generating being diff every time
    Co-authored-by: mat <git@matdoes.dev>

--- oneclient/generate-bundles.sh
+++ oneclient/generate-bundles.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Checking for required deps"
+for cmd in zip unzip wget; do
+  if ! command -v "$cmd" >/dev/null 2>&1; then
+    echo "Error: Required command '$cmd' is not installed or not in PATH." >&2
+    exit 1
+  fi
+done
+
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
-URL="https://nightly.link/Polyfrost/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
-ZIP="Linux-64-bit-x86.zip"
 
 echo "Downloading and setting up packwiz"
+URL="https://nightly.link/Polyfrost/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
+ZIP="Linux-64-bit-x86.zip"
 wget -O "$ZIP" "$URL" && unzip -o "$ZIP" && chmod +x packwiz
+
+echo "Creating required paths"
 mkdir -p generated
+export_dir="generated/export"
 
 for version in mrpacks/*; do
   [ -d "$version" ] || continue
@@ -17,6 +28,19 @@ for version in mrpacks/*; do
     echo "Bundling $bundle"
     name=$(basename "$bundle")
     name="${name,,}"
-    (cd "$bundle" && ../../../packwiz modrinth export --output ../../../generated/$name-$parsed.mrpack)
+    output="generated/$name-$parsed.mrpack"
+    (cd "$bundle" && ../../../packwiz modrinth export --output "../../../$output")
+
+    # Rezip it so the sorting is the same every time
+    # Reset all the timestamps to 0 (unix) so they don't change
+    echo "Fixing Bundle $bundle"
+    rm -rf "$export_dir"
+    mkdir -p "$export_dir"
+    unzip -q "$output" -d "$export_dir"
+    rm "$output"
+    find "$export_dir" -exec touch -h -d '@0' {} +
+    (cd "$export_dir" && LC_ALL=C find . -print | sort | zip -X -q -@ "../$(basename "$output")")
+    rm -rf "$export_dir"
   done
 done
+
